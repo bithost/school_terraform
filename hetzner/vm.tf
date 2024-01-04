04ac30aad2b4ab525b9abae513cdba33a69744dc
Tadas <tadasa@gmail.com>
2024-01-04 19:09:46 +0000

    let's see if vm will get IP address

--- hetzner/vm.tf
+++ hetzner/vm.tf
@@ -14,7 +14,7 @@ resource "hcloud_server" "web" {
   }
     network {
     network_id = hcloud_network.private-lan.id
-    ip = "10.10.1.5"
+    ip = "10.10.1.${count.index + 1}"
     }
     
   # **Note**: the depends_on is important when directly attaching the
