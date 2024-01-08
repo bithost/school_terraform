f550be78eabbf4c12aa85f6640b7df9817401c72
Tadas <tadasa@gmail.com>
2024-01-08 16:56:47 +0000

    changed ip range

--- hetzner/vm.tf
+++ hetzner/vm.tf
@@ -14,7 +14,7 @@ resource "hcloud_server" "web" {
   }
     network {
     network_id = hcloud_network.private-lan.id
-    ip = "10.10.1.${min(count.index + 1, 10)}"
+    ip = "10.10.1.1${min(count.index + 1, 10)}"
     }
     
   # **Note**: the depends_on is important when directly attaching the
